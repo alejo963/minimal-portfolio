9685327f4923c97f3ae20859f54abe929c9e9074
Alejandro Velásquez <v.alejo96@gmail.com>
2024-05-28 22:22:46 -0500

    Initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Ontology Population with Deep Learning and NPL",
+    imgUrl: "/assets/COGAF_Ontology.svg",
+    stack: ["Python", "PyTorch", "spaCy", "Pandas"],
+    link: "https://github.com/alejo963/cogaf-population",
+  },
+  {
+    title: "Project 2",
+    imgUrl: "",
+    stack: ["Langs", "Frameworks", "Libraries", "DBs", "etc"],
+    link: "<Add Project URL>",
+  },
+  {
+    title: "Project 3",
+    imgUrl: "",
+    stack: ["Langs", "Frameworks", "Libraries", "DBs", "etc"],
+    link: "<Add Project URL>",
+  },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    startYear: "2022",
+    endYear: "Today",
+    title: "Masters Degree in Data Science and Analysis",
+    duration: "2.5 years",
+    details:
+      "Wanting to apply both my work experience in programming and my knowledge in maths and statistics that I learned in college, I pursuing a career in Machine Learning and Data Science. Currently, I'm working on my final project involving Neural Networks, Natural Language Processing (NLP) and Ontologyt Learning.",
+  },
+  {
+    startYear: "2021",
+    endYear: "Today",
+    title: "Development Analyst in Cidenet SAS",
+    duration: "2 years",
+    details:
+      "I help build several projects of software and web development. This involved using different technologies including (but no limited to) TypeScript, Angular, Vue, NestJS, Python, MongoDB and Postgres",
+  },
+  {
+    startYear: "2021",
+    endYear: "2021",
+    title: "Data Analyst in Digital Americas Pipieline Initiative",
+    duration: "6 months",
+    details:
+      "My first job! Here I worked as a contractor, helping the company's AI team building solutions related to data analysis, ETL processes, etc",
+  },
+  {
+    startYear: "2015",
+    endYear: "2020",
+    title: "Engineering Physics Degree",
+    duration: "5 years",
+    details:
+      "With a passion for maths and physics and wanting to solve real world problems, I pursued a career in engineering physics. During this period, I discovered the wonderful world of programming.",
+  },
+];
